4ef128171b5af91cc86f8ddba163e0acf97c8324
unknown <roberthardales@hotmail.com>
2022-08-03 23:19:44 -0500

    144 3 agosto 23:19

--- applications/lector/views.py
+++ applications/lector/views.py
@@ -1,8 +1,11 @@
+from datetime import date
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.edit import FormView
 
+from .models import Prestamo
 from .forms import PrestamoForm
 
 class RegistrarPrestamo(FormView):
@@ -12,7 +15,21 @@ class RegistrarPrestamo(FormView):
     
     def form_valid(self,form):
         
-        
+        # Prestamo.objects.create(
+        #     lector=form.cleaned_data['lector'],
+        #     libro=form.cleaned_data['libro'],
+        #     fecha_prestamo=date.today(),
+        #     devuelto=False,
+        # )
+        prestamo= Prestamo(
+            lector=form.cleaned_data['lector'],
+            libro=form.cleaned_data['libro'],
+            fecha_prestamo=date.today(),
+            devuelto=False,
+
+        )
+
+        prestamo.save()
 
         return super(RegistrarPrestamo,self).form_valid(form)
 
